4d04c0aa14a25905adf384cebb492a6af28e684b
Elnura <elnura.sarkobenova.kk@gmail.com>
2022-01-28 18:12:29 +0100

    feat: add script4.js

---
+++ script4.js
@@ -0,0 +1,7 @@
+//4
+let suvenir = prompt('Suvenir');
+let bezdulish = prompt('Bezdelushka');
+let suvenir2 = suvenir*75;
+let bezdulish2 = bezdulish*112;
+
+document.write("Общая покупка: "+(suvenir2+bezdulish2)+" гр"+"<br>");
